83753e366ed0323f3e3d116f82c728c7d210496a
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-12-28 15:00:29 +0100

    make navbar more responsive

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,4 +1,4 @@
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, Menu } from "lucide-react";
 import BrandLogo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 import CartPopup from "./CartPopup";
@@ -20,14 +20,15 @@ const Navbar = ({ displayedPageName }) => {
         id="main-navbar"
         aria-label="Main"
         className="fixed inset-0 z-10 flex h-20 w-full items-center justify-between
-      border-b-2 border-b-background-700/40 bg-background-950/60 px-44 shadow-2xl backdrop-blur-2xl"
+      border-b-2 border-b-background-700/40 bg-background-950/60 px-44 shadow-2xl backdrop-blur-2xl max-lg:px-20
+        max-sm:hidden"
       >
         <Link to={"/home"}>
           <div className="flex h-12 w-24 items-center justify-center bg-accent-400 p-2">
             <img src={BrandLogo} alt="Logo of soz." />
           </div>
         </Link>
-        <ul className="flex items-center justify-center gap-28 text-xl font-semibold">
+        <ul className="flex items-center justify-center gap-28 text-xl font-semibold max-lg:gap-20">
           <Link to={"/home"}>
             <li
               className={`cursor-pointer transition-colors ${getTextColorClasses("home")}`}
@@ -49,6 +50,11 @@ const Navbar = ({ displayedPageName }) => {
             <ShoppingCart size={24} />
           </li>
         </ul>
+      </nav>
+      <nav className="fixed inset-0 z-10 flex h-12 w-full items-center px-3 sm:hidden">
+        <div className="mt-3 cursor-pointer rounded-xl bg-black/80 p-1.5 backdrop-blur-lg">
+          <Menu size={28} />
+        </div>
       </nav>
       <CartPopup />
     </>
