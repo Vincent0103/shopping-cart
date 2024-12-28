5d2ca3f992b52645837b57baf96138ca1bb677f0
Vincent0103 <vincentkanagasundaram@gmail.com>
2024-12-28 16:42:49 +0100

    make shop more responsive

--- src/components/shop/Card.jsx
+++ src/components/shop/Card.jsx
@@ -5,15 +5,16 @@ import { ShoppingCart } from "lucide-react";
 const Card = ({ id, imgSrc, title, price }) => (
   <Link to={`/product/${id}`}>
     <div
-      className="group relative flex h-96 w-72 cursor-pointer flex-col
-      items-center justify-center overflow-hidden rounded-lg border-2
-      border-background-200 bg-background-900/80"
+      className="group relative flex h-96 w-72 cursor-pointer flex-col items-center justify-center
+      overflow-hidden rounded-lg border-2 border-background-200 bg-background-900/80
+      max-lg:h-60 max-lg:w-48 max-sm:h-32 max-sm:w-24 max-sm:border-none"
     >
       <div
-        className="flex h-72 max-h-72 w-full items-center justify-center
-        rounded-t-md bg-gradient-to-b from-background-400 to-background-800"
+        className="flex h-72 max-h-72 w-full items-center justify-center rounded-t-md bg-gradient-to-b
+        from-background-400 to-background-800 max-lg:h-48 max-lg:max-h-48
+        max-sm:h-24 max-sm:max-h-24"
       >
-        <div className="h-72">
+        <div className="h-72 max-lg:h-48 max-sm:h-20">
           <img
             className="h-full rounded-t-md object-cover"
             src={imgSrc}
@@ -21,13 +22,24 @@ const Card = ({ id, imgSrc, title, price }) => (
           />
         </div>
       </div>
-      <div className="pointer-events-none absolute inset-0 h-72 max-h-72 w-full bg-black opacity-0 group-hover:opacity-50"></div>
-      <div className="grid size-full grid-cols-2 grid-rows-2 gap-3 overflow-hidden border-t-2 border-t-background-200 px-4 py-2">
-        <h5 className="col-start-1 col-end-3 self-end overflow-hidden text-ellipsis text-nowrap text-lg font-medium">
+      <div
+        className="pointer-events-none absolute inset-0 h-72 
+        max-h-72 w-full bg-black opacity-0 group-hover:opacity-50
+        max-lg:h-48 max-lg:max-h-48 max-sm:h-20 max-sm:max-h-20"
+      ></div>
+      <div
+        className="grid size-full grid-cols-2 grid-rows-2 gap-3
+        overflow-hidden border-t-2 border-t-background-200 px-4
+        py-2 max-lg:gap-1.5 max-sm:gap-0.5"
+      >
+        <h5
+          className="col-start-1 col-end-3 self-end overflow-hidden 
+          text-ellipsis text-nowrap text-lg max-lg:text-sm"
+        >
           {title}
         </h5>
         <p className="h-min text-sm">{price}</p>
-        <div className="flex size-full items-start justify-end">
+        <div className="flex size-full items-start justify-end max-sm:hidden">
           <button type="button">
             <ShoppingCart size={16} />
           </button>
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 const Li = ({ text, linkTo, isSelected = false }) => (
   <Link className="flex size-full items-center justify-center" to={linkTo}>
     <li
-      className={`flex size-full items-center justify-end rounded-md px-6 py-3 transition-colors duration-100
+      className={`flex size-full items-center justify-end rounded-md px-4 py-2 transition-colors duration-100 max-md:px-2 max-md:py-1
           ${isSelected ? "bg-gradient-to-r from-accent-200 to-accent-400 text-text-950 shadow-lg" : "hover:bg-accent-200/50 hover:text-text-950"}`}
     >
       <h3>{text}</h3>
@@ -29,12 +29,18 @@ const Shop = () => {
   return (
     <div className="size-full">
       <div className="relative top-20 size-full">
-        <nav aria-label="Category" className="fixed top-20 w-[430px] p-3">
+        <nav
+          aria-label="Category"
+          className="fixed top-20 w-[430px] p-3 max-xl:w-[350px] max-md:w-[200px] max-md:p-1.5"
+        >
           <div className="min-h-calc-100vh-96px w-full rounded-xl bg-accent-900 shadow-xl">
-            <div className="flex justify-end rounded-xl bg-accent-500 py-6 pr-10 shadow-xl">
-              <h3 className="text-4xl font-black">Category</h3>
+            <div className="flex justify-end rounded-xl bg-accent-500 py-4 pr-6 shadow-xl max-md:rounded-md max-md:py-2 max-md:pr-3">
+              <h3 className="text-2xl font-bold max-md:text-lg">Category</h3>
             </div>
-            <ul className="flex flex-col items-center justify-center gap-2 p-1 py-3 text-3xl font-semibold">
+            <ul
+              className="flex flex-col items-center justify-center gap-2 p-1 py-3 text-2xl font-semibold max-md:py-1 
+              max-md:text-base max-md:font-medium"
+            >
               <Li text={"All"} linkTo={"/shop"} isSelected={name === ""} />
               <Li
                 text={"Electronics"}
@@ -59,7 +65,10 @@ const Shop = () => {
             </ul>
           </div>
         </nav>
-        <section className="relative flex flex-wrap w-full-minus-left-430px gap-8 p-3 left-[430px]">
+        <section
+          className="relative left-[430px] flex w-full-minus-left-430px flex-wrap justify-center gap-8 p-3 max-xl:left-[350px]
+          max-xl:w-full-minus-left-350px max-md:left-[200px] max-md:w-full-minus-left-200px"
+        >
           <Category categoryName={name} />
         </section>
       </div>
